6e753acf06c0e2b7d1c520c15e2e8159ed38ae57
Vishal Dixit <dixitvishal1998@gmail.com>
2020-09-19 18:05:50 +0530

    Basic code added

--- Arrays/DuplicatesInArray/main.cpp
+++ Arrays/DuplicatesInArray/main.cpp
@@ -1,6 +1,30 @@
 #include <bits//stdc++.h>
+using namespace std;
 
 int main() {
-    //iniial code
+    /*Basic code with keeping count using Map || Freq Array
+     We can also use a set to just store the Unique element.
+     */
+    int n;
+    cin>>n;
+
+    int a[n];
+    map<int,int> m;
+    for(int i=0;i<n;i++) {
+        cin >> a[i];
+        m[a[i]]++;
+    }
+
+    int ans=0;
+    for(auto i=m.begin();i!=m.end();i++)
+    {
+        if(i->second>1)
+        {
+            ans=i->first;
+            break;
+        }
+    }
+
+    cout<<ans<<endl;
     return 0;
 }
