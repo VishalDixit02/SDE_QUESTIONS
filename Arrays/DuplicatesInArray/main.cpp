017fd8244334d095c8b40f39cecd933af3f80497
Vishal Dixit <dixitvishal1998@gmail.com>
2020-09-19 19:42:28 +0530

    New code for the same question

--- Arrays/DuplicatesInArray/main.cpp
+++ Arrays/DuplicatesInArray/main.cpp
@@ -2,20 +2,21 @@
 using namespace std;
 
 int main() {
-    /*Basic code with keeping count using Map || Freq Array
-     We can also use a set to just store the Unique element.
-     */
     int n;
     cin>>n;
 
     int a[n];
+    int ans=0;
+    for(int i=0;i<n;i++)
+        cin >> a[i];
+    /*Basic code with keeping count using Map || Freq Array
+     We can also use a set to just store the Unique element.
+
     map<int,int> m;
     for(int i=0;i<n;i++) {
         cin >> a[i];
         m[a[i]]++;
     }
-
-    int ans=0;
     for(auto i=m.begin();i!=m.end();i++)
     {
         if(i->second>1)
@@ -24,7 +25,19 @@ int main() {
             break;
         }
     }
+     */
 
-    cout<<ans<<endl;
+    /* We can also use Sum method
+     * Add all the numbers in a variable and then subtract the sum from n*(n+1)/2
+     */
+
+    int sum1=0;
+    for(int i=0;i<n;i++)
+        sum1+=a[i];
+
+    int sum2=(n*(n+1))/2;
+
+    int diff=sum1-sum2;
+    cout<<diff<<endl;
     return 0;
 }
