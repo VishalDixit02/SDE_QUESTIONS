e2cfa1d06fa39b972d8162b97b24340ac3fda9ec
Vishal Dixit <dixitvishal1998@gmail.com>
2020-09-22 23:02:49 +0530

    MergerSort two arrays

--- Arrays/MergeSortedArrays/main.cpp
+++ Arrays/MergeSortedArrays/main.cpp
@@ -5,14 +5,31 @@ int main() {
     int n,m;
     cin>>n>>m;
 
+    //Vector a has enough space to store the remaining element
     int a[n];
-    int b[n];
+    int b[m];
     for(int i=0;i<n;i++)
         cin>>a[i];
     for(int i=0;i<m;i++)
         cin>>b[i];
 
-    vector<int> c;
+    //Simple method to solve the question using three pointers.
+    int i=n-1;
+    int j=m-1;
+    int k=m+n-1;
+
+    while(i>=0 && j>=0)
+    {
+        if(a[i]>b[j])
+            a[k--]=a[i--];
+        else
+            a[k--]=b[j--];
+    }
+
+    while(j>=0)
+    {
+        a[k--]=b[j--];
+    }
     
     return 0;
 }
